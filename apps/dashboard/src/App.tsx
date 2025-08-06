1903ea5f7451d59ff00e850dcf7172f836ca6782
Сергей Прасолов <prasol555555@gmail.com>
2025-08-06 12:10:34 +0300

    connect all

--- apps/dashboard/src/App.tsx
+++ apps/dashboard/src/App.tsx
@@ -1,4 +1,21 @@
+import {useInitAuth} from "./hooks/useInitAuth";
+import {useAppSelector} from "./slices/hooks";
+import AppRouter from "./AppRouter";
+import {Navigate} from "react-router-dom";
+
 
 export default function App() {
-  return <h1>Hello from Dashboard</h1>
+  useInitAuth();
+
+  const {user, isLoading, isAuthenticated} = useAppSelector(state => state.auth);
+  console.log(user);
+  if (isLoading) return <div>Loading...</div>;
+  if (!isAuthenticated) return <Navigate to='http://localhost:3000/login' />;
+
+  return (
+      <>
+        <AppRouter/>
+      </>
+  )
+
 }
--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,11 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { StrictMode } from 'react';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./slices";
 
 
 const rootElement = document.getElementById('root') as HTMLElement;
 createRoot(rootElement).render(
   <StrictMode>
-      <App />
+      <BrowserRouter>
+          <Provider store={store}>
+              <App />
+          </Provider>
+      </BrowserRouter>
   </StrictMode>,
 );
